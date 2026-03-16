61a513e77cae041da7ba8061261d569c26ca4e58
Eldorado Daniel <140812991+eldoradonode@users.noreply.github.com>
2026-03-17 00:06:55 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
-ts
-
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     base: '/',
     plugins: [react(), tailwindcss()],
